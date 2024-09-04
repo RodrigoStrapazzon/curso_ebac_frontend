1b27a9e29abd93eb918d737b279d67d251f4e964
Rodrigo Strapazzon <114418794+RodrigoStrapazzon@users.noreply.github.com>
2024-09-04 11:32:37 -0300

    Create scripts.ts

---
+++ scripts.ts
@@ -0,0 +1,13 @@
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+function saudar(nome: string): string {
+    return `Olá ${nome}`;
+}
+
+const resultadoMultiplicacao = multiplicar(5, 3);
+const saudacao = saudar("Rodrigo");
+
+console.log(resultadoMultiplicacao);
+console.log(saudacao);
